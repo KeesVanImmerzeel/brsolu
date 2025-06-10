9ee87a81264b609e9f798e71cd8fd61867d016b5
Kees van Immerzeel <c.h.van.immerzeel@gmail.com>
2025-06-10 11:57:58 +0200

    brsolu.r added

--- R/B1_one_dimensional_flow.R
+++ R/B1_one_dimensional_flow.R
@@ -15,9 +15,6 @@
 #' @param t Time (T)
 #' @inheritParams Labda
 #' @inheritParams Eta
-# @param S Storage coefficient [-]
-# @param kD Hydraulic conductivity [L2/T]
-# @param c Hydraulic resistance [T]
 #' @param h Constant in the definition of the stress (= sudden drawdown) (L)
 #' @return Phi Head change (L)
 #' @export
@@ -29,9 +26,13 @@ Br_123_32 <- function(x, t, S, kD, c, h) {
 }
 
 #' Bruggeman Formula 123.32 p. 66
-#' First Derivative with respect to x [-]
-#d_Br_123_32_dx <- function(x, t, S, kD, c, h) {
-#  Labda = sqrt(c * kD)
-#  Eta = 1 / (c * S)
-#  h * brmath::dPdx(x / (2 * Labda), sqrt(Eta * t)) / (2 * Labda)
-#}
+
+#' First Derivative of Phi with respect to x
+#' @inheritParams Br_123_32
+#' @return First Derivative of Phi with respect to x (-)
+#' @export
+d_Br_123_32_dx <- function(x, t, S, kD, c, h) {
+  labda = Labda(c, kD)
+  eta = Eta(c, S)
+  h * brmath::dPdx(x / (2 * labda), sqrt(eta * t)) / (2 * labda)
+}
---
+++ R/brsolu.r
@@ -0,0 +1,17 @@
+#' Analytical Solutions of Geohydrological Problems.
+#'
+#' Functions as defined in Part A ("Mathematical tools") in the book
+#' "Analytical solutions of geohydrological problems" by G.A. Bruggeman.
+#'
+#' @section Chapter B1: One-dimensional grondwater flow:
+#'
+#' \code{\link{Br_123_32}}
+#'
+#' \code{\link{d_Br_123_32_dx}}
+#'
+#' @name brsolu
+#'
+#' @importFrom brmath P
+#' @importFrom brmath dPdx
+#'
+NULL
